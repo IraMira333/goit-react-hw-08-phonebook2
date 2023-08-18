d46433466d488751e324999b84dc314777438fe1
IraMira333 <irinat333@gmail.com>
2023-08-18 16:30:12 +0300

    Create authSlice.js

---
+++ src/redux/auth/authSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { getCurrentUser, logIn, logOut, register } from './authOperations';
+
+const initialState = {
+  user: { name: null, email: null },
+  token: null,
+  isLoggedIn: false,
+  isRefreshing: false,
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+
+  extraReducers: builder => {
+    builder
+      .addCase(register.fulfilled, (state, action) => {
+        console.log(action.payload);
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(logIn.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(logOut.fulfilled, (state, action) => {
+        state.user = { name: null, email: null };
+        state.token = null;
+        state.isLoggedIn = false;
+      })
+      .addCase(getCurrentUser.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.isLoggedIn = true;
+      });
+  },
+});
+
+export const authSliceReducer = authSlice.reducer;
